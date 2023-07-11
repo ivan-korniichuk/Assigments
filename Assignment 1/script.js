9b4aab403d941cd7c995d3bfabb515d965936745
IKFor <85750192+IKFor@users.noreply.github.com>
2023-07-11 21:29:57 +0100

    new code for assignment 1

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,40 +1,81 @@
-const previousRequests = localStorage.getItem("previousRequests") ? 
-JSON.parse(localStorage.getItem("previousRequests")) :
-[];
-console.log(previousRequests);
-displayPreviousRequests();
-document.querySelector("#start-date").addEventListener("input", () => {
-    if(validDate(document.querySelector("#start-date").value)){
-        document.querySelector("#end-date").disabled = false;
-        document.querySelector("#end-date").min = document.querySelector("#start-date").value;
-        document.querySelector("#add-week").classList.remove("isDisabled");
-        document.querySelector("#add-month").classList.remove("isDisabled");
-    }
-    else{
-        document.querySelector("#end-date").disabled = true;
-        document.querySelector("#add-week").classList.add("isDisabled");
-        document.querySelector("#add-month").classList.add("isDisabled");
-    }
-});
-for(let preset of document.querySelectorAll("a.preset")){
+const endDateInput = document.getElementById("end-date");
+const startDateInput = document.getElementById("start-date");
+const addWeekPreset = document.getElementById("add-week");
+const addMonthPreset = document.getElementById("add-month");
+const form = document.querySelector(".date-counter-form");
+const presets = document.querySelectorAll("a.preset");
+const timeRadios = document.getElementsByName("time-units");
+const daysRadios = document.getElementsByName("included-days");
+const previousRequestElements = document.querySelector(".previous-requests")
+
+renderPreviousRequests();
+
+startDateInput.addEventListener("input", enableEndInputOnValid);
+form.addEventListener("submit", trySubmitForm);
+
+for(let preset of presets) {
     preset.addEventListener("click", () => {
-        document.querySelector("#end-date").value = dateFromPresset(document.querySelector("#start-date").value, preset.getAttribute("data-preset"));
+        endDateInput.value = dateFromPresset(startDateInput.value, preset.getAttribute("data-preset"));
     });
 }
-document.querySelector(".date-counter-form").addEventListener("submit", (event) => {
-    const startDate = document.querySelector("#start-date").value;
-    const endDate = document.querySelector("#end-date").value;
-    const result = getDuration(
-        startDate,
-        endDate,
-        getRadioValue(document.getElementsByName("time-units")),
-        getRadioValue(document.getElementsByName("included-days"))
-        );
-    event.preventDefault();
-    if (document.querySelector(".date-counter-form").checkValidity())  {
-        createSavedCounter(startDate, endDate, result);
+
+function setRequestsToLocalStorage(previousRequests) {
+    localStorage.setItem("previousRequests", JSON.stringify(previousRequests));
+}
+
+function getRequestsFromLocalStorage() {
+    return localStorage.getItem("previousRequests") !== null ? JSON.parse(localStorage.getItem("previousRequests")) : [];
+}
+
+function addNewRequestToLocalStorage(startDate, endDate, result) {
+    let previousRequests = getRequestsFromLocalStorage();
+
+    previousRequests.push({
+        startDate: startDate,
+        endDate: endDate,
+        result: result,
+    });
+
+    while(previousRequests.length > 10){
+        previousRequests.shift();
     }
-});
+
+    setRequestsToLocalStorage(previousRequests);
+
+    renderPreviousRequests();
+}
+
+function enableEndInputOnValid() {
+    if(validDate(startDateInput.value)){
+        endDateInput.disabled = false;
+        endDateInput.min = startDateInput.value;
+        addWeekPreset.classList.remove("isDisabled");
+        addMonthPreset.classList.remove("isDisabled");
+    }
+    else{
+        endDateInput.disabled = true;
+        addWeekPreset.classList.add("isDisabled");
+        addMonthPreset.classList.add("isDisabled");
+    }
+}
+
+function trySubmitForm (event) {
+    event.preventDefault();
+
+    if (form.checkValidity()) {
+        const startDate = startDateInput.value;
+        const endDate = endDateInput.value;
+        const result = getDuration(
+            startDate,
+            endDate,
+            getRadioValue(timeRadios),
+            getRadioValue(daysRadios)
+        );
+
+        addNewRequestToLocalStorage(startDate, endDate, result);
+    }
+}
+
 function dateFromPresset(date, preset = "week"){
     let newDate;
     switch (preset){
@@ -47,6 +88,7 @@ function dateFromPresset(date, preset = "week"){
     }
     return newDate.getFullYear() + "-" + String(newDate.getMonth() + 1).padStart(2, '0') + "-" + String(newDate.getDate()).padStart(2, '0');
 }
+
 function getDuration(startDate, endDate, units = "days", type = "all days"){
     const duration = convertTime((Date.parse(endDate) - Date.parse(startDate)), "milliseconds", units)
     let result = "";
@@ -65,6 +107,7 @@ function getDuration(startDate, endDate, units = "days", type = "all days"){
     result += " " + units;
     return result;
 }
+
 function convertTime(time, unit = "days", newUnit = "days") {
     switch (unit) {
         case "days":
@@ -102,16 +145,17 @@ function convertTime(time, unit = "days", newUnit = "days") {
     }
     return time;
 }
+
 function getWeekendDays(startDate, endDate){
     let days = convertTime((Date.parse(endDate) - Date.parse(startDate)), "milliseconds", "days")
     let result = 0;
     startDate = new Date(Date.parse(startDate)).getDay();
     endDate = new Date(Date.parse(endDate)).getDay();
+
     if (days === 0) {
         console.log("days === 0");
         return 0;
-    }
-    else if (days < 7) {
+    } else if (days < 7) {
         if (startDate < endDate) {
             if (startDate === 0) {
                 return 1;
@@ -120,33 +164,18 @@ function getWeekendDays(startDate, endDate){
             }
         } 
     }
-    if (startDate === 0) {
-        result += 1;
-    }
+
+    if (startDate === 0) result += 1;
+
     if (endDate === 0) {
         result += 1;
     } else {
         result += 2;
     }
+
     days -= 7 - startDate + endDate;
     result += days / 7 * 2;
     return result;
-}
-
-function createSavedCounter(startDate, endDate, result) {
-    previousRequests.push({
-        startDate: startDate,
-        endDate: endDate,
-        result: result,
-    });
-    while(previousRequests.length > 10){
-        previousRequests.shift();
-    }
-    localStorage.setItem("previousRequests", JSON.stringify(previousRequests));
-    console.clear();
-    console.log(JSON.parse(localStorage.getItem("previousRequests")));
-    displayPreviousRequests();
-    //location.reload;
 }
 
 function validDate(date){
@@ -162,16 +191,18 @@ function getRadioValue(elements){
     return false;
 }
 
-function displayPreviousRequests(){
+function renderPreviousRequests(){
+    const previousRequests = getRequestsFromLocalStorage();
     let items = "";
-    for(previousRequest of previousRequests){
+
+    for(let previousRequest of previousRequests){
         items += `
         <div class="previous-request">
-            <div class="previous-data">Start date: ${previousRequest.startDate}</div>
-            <div class="previous-data">End date: ${previousRequest.endDate}</div>
-            <div class="previous-data">Result: ${previousRequest.result}</div>
+            <span class="previous-data">Start date: ${previousRequest.startDate}</span>
+            <span class="previous-data">End date: ${previousRequest.endDate}</span>
+            <span class="previous-data">Result: ${previousRequest.result}</span>
         </div>
         `
     }
-    document.querySelector(".previous-requests").innerHTML = items;
+    previousRequestElements.innerHTML = items;
 }
